3c575ea3a92b4c03c943b4e72979505b320bd155
laj <larry.jones@reveal-energy.com>
2022-08-28 15:23:54 -0500

    Inference of `forEach` and `map`

--- ts-basics/basics.ts
+++ ts-basics/basics.ts
@@ -38,3 +38,26 @@ if (ids[30] === 'D') {
 for(let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+// Inference determines that `i` is of type `number`.
+// I do not understand the optional indicator on `i`.
+// Perhaps it applies to an empty array.
+[1, 2, 3].forEach(i => console.log(i));
+
+// Inference works for objects, too!
+[{ first: 'Larry', last: 'Jones' },
+    { first: 'Jack', last: 'Herrington' }].forEach(p => console.log(p));
+
+// Similar comments apply to `map`.
+// Note that inference correctly determines the result of the call to `map`
+const out: number[] = [1, 2, 3].map(i => i + 3);
+console.log(out);
+
+// If we change the function supplied to map...
+// TypeScript correctly determines the result type.
+const outAgain: string[] = [1, 2, 3].map(n => `${n * 10}`)
+console.log(outAgain);
+
+// Determines mismatch between expected result type and actual
+// Uncomment to see error.
+// const stillOut: number[] = [1, 2, 3].map(i => `${i + 7}`);
